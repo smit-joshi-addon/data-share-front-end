4f5aa77e5a1686916af79ad8167b43e9c7c6fc83
Smit Joshi <intern-j05@addontechnologies.net>
2024-08-09 18:15:16 +0530

    basic froent-end for data-sharing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,70 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ECommerceComponent } from './e-commerce/e-commerce.component';
-import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BusinessComponent } from './business/business.component';
+import { DataMasterComponent } from './data-master/data-master.component';
+import { DataDetailComponent } from './data-detail/data-detail.component';
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
+
     {
       path: 'dashboard',
-      component: ECommerceComponent,
-    },
-    {
-      path: 'iot-dashboard',
       component: DashboardComponent,
     },
+
     {
-      path: 'layout',
-      loadChildren: () => import('./layout/layout.module')
-        .then(m => m.LayoutModule),
+      path: 'business',
+      component: BusinessComponent,
     },
     {
-      path: 'forms',
-      loadChildren: () => import('./forms/forms.module')
-        .then(m => m.FormsModule),
+      path: 'data-master',
+      component: DataMasterComponent,
     },
     {
-      path: 'ui-features',
-      loadChildren: () => import('./ui-features/ui-features.module')
-        .then(m => m.UiFeaturesModule),
-    },
-    {
-      path: 'modal-overlays',
-      loadChildren: () => import('./modal-overlays/modal-overlays.module')
-        .then(m => m.ModalOverlaysModule),
-    },
-    {
-      path: 'extra-components',
-      loadChildren: () => import('./extra-components/extra-components.module')
-        .then(m => m.ExtraComponentsModule),
-    },
-    {
-      path: 'maps',
-      loadChildren: () => import('./maps/maps.module')
-        .then(m => m.MapsModule),
-    },
-    {
-      path: 'charts',
-      loadChildren: () => import('./charts/charts.module')
-        .then(m => m.ChartsModule),
-    },
-    {
-      path: 'editors',
-      loadChildren: () => import('./editors/editors.module')
-        .then(m => m.EditorsModule),
-    },
-    {
-      path: 'tables',
-      loadChildren: () => import('./tables/tables.module')
-        .then(m => m.TablesModule),
-    },
-    {
-      path: 'miscellaneous',
-      loadChildren: () => import('./miscellaneous/miscellaneous.module')
-        .then(m => m.MiscellaneousModule),
+      path: 'data-detail',
+      component: DataDetailComponent,
     },
     {
       path: '',
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbInputModule, NbMenuModule, NbRadioModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
+
 import { PagesRoutingModule } from './pages-routing.module';
-import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { FormsModule as ngFormsModule } from '@angular/forms';
+import { BusinessComponent } from './business/business.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DataMasterComponent } from './data-master/data-master.component';
+import { DataDetailComponent } from './data-detail/data-detail.component';
 
 @NgModule({
   imports: [
@@ -14,11 +19,19 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
     ThemeModule,
     NbMenuModule,
     DashboardModule,
-    ECommerceModule,
-    MiscellaneousModule,
+    NbCardModule,
+    ngFormsModule,
+    NbInputModule,
+    NbButtonModule,
+    NbRadioModule,
+    Ng2SmartTableModule,
   ],
   declarations: [
     PagesComponent,
+    BusinessComponent,
+    DataMasterComponent,
+    DataDetailComponent,
+    NotFoundComponent
   ],
 })
 export class PagesModule {
